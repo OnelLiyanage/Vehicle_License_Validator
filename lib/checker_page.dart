b1ee2bc4f3994eee97c3ccce3edec23b366e558b
OnelLiyanage <onel111liyanage@gmail.com>
2022-05-21 02:00:54 +0530

    Initial commit

---
+++ lib/checker_page.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+
+class checkerPage extends StatefulWidget {
+  const checkerPage({ Key? key }) : super(key: key);
+
+  @override
+  State<checkerPage> createState() => _checkerPageState();
+}
+
+class _checkerPageState extends State<checkerPage> {
+    TextEditingController licenseNumber = TextEditingController();
+    checkValidity () {
+
+        String numberLicense = licenseNumber.text.replaceAll(RegExp(r"\s+\b|\b\s"), "");
+        numberLicense =numberLicense.toUpperCase();
+        int licenseLength = numberLicense.length;
+        print(numberLicense);
+        print(licenseLength);
+
+        //  7 digit license plate validation
+        if (licenseLength == 7) {
+
+            String sectionOne = numberLicense.substring(0,2);
+            String sectionTwo = numberLicense.substring(2,3);
+            String sectionThree = numberLicense.substring(3,7);
+
+            if (sectionOne.contains(RegExp(r"^[0-9]+[0-9]")) && sectionTwo.contains(RegExp(r"^[-]")) && sectionThree.contains(RegExp(r"^[0-9]+[0-9]+[0-9]+[0-9]")) ) {
+                print("Old License");
+                print("--------------------------");
+            
+            showDialog(context: context, builder: (BuildContext context){ 
+                        return AlertDialog(
+                            title:  Center(
+                                child: Text("Old License")
+                            ),
+                            content: Text('"'+numberLicense+'"'+" is an older license number.\nCompletely Valid.")
+                        );
+            });
+            } else {
+                print("Invalid");
+                print("--------------------------");
+                showDialog(context: context, builder: (BuildContext context){ 
+                    return AlertDialog(
+                        title:  Center(
+                            child: Text("Invalid Number")
+                        ),
+                        content: Text('"'+numberLicense+'"'+" is not a license number.\nCompletely Invalid.")
+                    );
+                });
+            }
+        } else {
+            print("Not in range for 7");
+            print("--------------------------");
+        }
+
+        //  8 digit license plate validation
+        if (licenseLength == 8) {
+          
+            String sectionOne = numberLicense.substring(0,3);
+            String sectionTwo = numberLicense.substring(3,4);
+            String sectionThree = numberLicense.substring(4,8);
+
+            if (sectionOne.contains(RegExp(r"^[0-9]+[0-9]+[0-9]")) && sectionTwo.contains(RegExp(r"^[-]")) && sectionThree.contains(RegExp(r"^[0-9]+[0-9]+[0-9]+[0-9]"))) {
+                print("Old License 2");
+                print("--------------------------");
+            showDialog(context: context, builder: (BuildContext context){ 
+                        return AlertDialog(
+                            title:  Center(child: Text("Old License")),
+                            content: Text('"'+numberLicense+'"'+" is an older license number.\nCompletely Valid.")
+                        );
+            });
+            } else {
+                showDialog(context: context, builder: (BuildContext context){ 
+                    return AlertDialog(
+                        title:  Center(
+                            child: Text("Invalid Number")
+                        ),
+                        content: Text('"'+numberLicense+'"'+" is not a license number.\nCompletely Invalid.")
+                    );
+                });
+            }
+
+            if (sectionOne.contains(RegExp(r"^[a-zA-Z]+[a-zA-Z]+[a-zA-Z]")) && sectionTwo.contains(RegExp(r"^[-]")) && sectionThree.contains(RegExp(r"^[0-9]+[0-9]+[0-9]+[0-9]"))) {
+                print("Modern");
+                print("--------------------------");
+                showDialog(context: context, builder: (BuildContext context){ 
+                    return AlertDialog(
+                        title:  Center(child: Text("New License")),
+                        content: Text('"'+numberLicense+'"'+" is an newer license number.\nCompletely Valid.")
+                    );
+                });
+            } else {
+                showDialog(context: context, builder: (BuildContext context){ 
+                    return AlertDialog(
+                        title:  Center(
+                            child: Text("Invalid Number")
+                        ),
+                        content: Text('"'+numberLicense+'"'+" is not a license number.\nCompletely Invalid.")
+                    );
+                });
+            }
+        }
+
+            //  9 digit license plate validation
+        if (licenseLength == 9) {
+
+            String sectionOne = numberLicense.substring(0,4);
+            String sectionTwo = numberLicense.substring(4,5);
+            String sectionThree = numberLicense.substring(5,9);
+
+            if (sectionOne.contains(RegExp(r"^[a-zA-Z]+[P]+[a-zA-Z]+[a-zA-Z]")) && sectionTwo.contains(RegExp(r"^[-]")) && sectionThree.contains(RegExp(r"^[0-9]+[0-9]+[0-9]+[0-9]"))) {
+                print("Modern");
+                print("--------------------------");
+                showDialog(context: context, builder: (BuildContext context){ 
+                    return AlertDialog(
+                        title:  Center(child: Text("New License")),
+                        content: Text('"'+numberLicense+'"'+" is an newer license number.\nCompletely Valid.")
+                    );
+                });
+            } else {
+                showDialog(context: context, builder: (BuildContext context){ 
+                    return AlertDialog(
+                        title:  Center(
+                            child: Text("Invalid Number")
+                        ),
+                        content: Text('"'+numberLicense+'"'+" is not a license number.\nCompletely Invalid.")
+                    );
+                });
+            }
+        } else {
+            print("Not in range for 9");
+            print("--------------------------");
+        }
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text (
+          "Registration Validation",
+          style: TextStyle (
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.green[700],
+        elevation: 0,
+      ),
+
+    body: Container (
+        alignment: Alignment.topCenter,
+            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 30.0),
+            child: Column (
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                    Form (
+                        child: Column (
+                            children: [
+                                TextFormField(
+                                    validator: (val) {
+                                        return val!.isEmpty || val.length < 5 ? null : "Invalid License number" ;
+                                    },
+                                    controller: licenseNumber,
+                                    decoration: const InputDecoration (
+                                        hintText: "License number here."
+                                        ),
+                                ),
+                            ]
+                        ),
+                    ),
+                    const SizedBox( height : 25.0),
+                    GestureDetector(
+                        onTap: () {
+                            checkValidity();
+                        },
+                        child: Container(
+                            alignment: Alignment.center,
+                            width: MediaQuery.of(context).size.width,
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            decoration: BoxDecoration(
+                                gradient: const LinearGradient (
+                                    colors:  [
+                                        Color.fromARGB(169, 49, 163, 139),
+                                        Color.fromARGB(178, 49, 163, 138), 
+                                        ]
+                                ),
+                                borderRadius: BorderRadius.circular(25)
+                            ),
+                            child: const Text (
+                                "Check Validity",
+                                style: TextStyle (
+                                    fontSize: 18.0,
+                                    color: Colors.white,
+                                ),
+                            ),
+                        ),
+                    ),
+                ],
+            ),
+        ),
+        
+    );
+}   }
+
